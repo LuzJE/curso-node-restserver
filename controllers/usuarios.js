2cf4e5e22a56715081cae0fb24b4231bdafd06d2
LuzJE <luzprojects1@gmail.com>
2022-09-01 17:42:35 -0500

    Primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,58 @@
+const { response, request} = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+
+    // son parámetros que se obtienen directamente de la url
+    //http://localhost:8080/api/usuarios?q=hola&nombre=luz&apikey=1234567890
+    const { q, nombre = "No name", apikey } = req.query; 
+
+    res.json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey
+    })
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const {nombre, edad} = req.body;
+
+    res.json({
+        msg: 'post API - usuariosPost',
+        nombre,
+        edad
+    })
+}
+
+const usuariosPut = (req, res = response) => {
+    
+    const { id } = req.params;
+
+    res.json({
+        msg: 'put API - usuariosPut',
+        id
+    })
+}
+
+const usuariosPatch = (req, res = response) => {
+
+    res.json({
+        msg: 'patch API - usuariosPatch'
+    })
+}
+
+const usuariosDelete = (req, res = response) => {
+
+    res.json({
+        msg: 'delete API - usuariosDelete'
+    })
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete
+}
---
+++ models/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+    
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        //  Middlewares son funciones que le van a agregar otra funcionalidad a mi web server, en
+        // otras palabras es una función que siempre va a ejecutarse cuando levantemos nuestro servidor
+        this.middlewares();
+
+        // Rutas de mi aplicación
+        this.routes();
+    }
+
+    middlewares() {
+        // "use" es la palabra clave para expresar que esto es un middleware
+        
+        // CORS 
+        this.app.use(cors());
+
+        // Lectura y parseo del body 
+        this.app.use(express.json());
+        
+        // Directorio Público
+        this.app.use(express.static("public"));
+    }
+
+    routes() {
+        this.app.use(this.usuariosPath, require('../routes/usuarios'));
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor en puerto', this.port);
+        });
+    }
+
+}
+
+module.exports = Server;
